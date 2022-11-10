4b1a4cb94d74df17a8d495ad4b1cd88ff0f4ee74
ManishBangari <manishbangari2108@gmail.com>
2022-11-10 19:47:41 +0530

    first commit

---
+++ Competitive_Programming_Template.cc
@@ -0,0 +1,34 @@
+
+/* Date Created : 10 November 2022
+
+Creator : Manish Bangari */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll; 
+typedef long l;
+
+#define PI 3.141592653589793238
+
+void solve(){
+
+
+    return ;
+}
+int main(){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t=0;
+    l a=0,b=0,c=0,x=0,y=0,z=0,p=0,q=0,r=0;
+
+    cin>>t;
+    while(t--){
+        cin>>   ;
+        solve();
+    }
+
+    return 0;
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+
+	/*
+	 	
+	This piece of code is a Competitive Programmimg Template
+	created on 10 November 2022 by Manish Bangari		
+	
+	*/
+
